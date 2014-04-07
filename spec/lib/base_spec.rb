b376159290550168c08d8045c2faf606e1fa357e
Josh Leitzel <joshleitzel@gmail.com>
2014-04-07 09:45:39 -0700

    Use correct syntax

--- spec/lib/base_spec.rb
+++ spec/lib/base_spec.rb
@@ -2,12 +2,12 @@ require 'spec_helper'
 
 describe Cathode::Base do
   describe '#resource' do
-    subject { Cathode::Base.resource(:boxes, [:all]) }
+    subject { Cathode::Base.resource(:boxes, actions: [:all]) }
 
     it 'initializes a new resource with the params' do
       expect(Cathode::Resource).to receive(:new) do |resource_name, params|
         expect(resource_name).to eq(:boxes)
-        expect(params).to eq([:all])
+        expect(params).to eq(actions: [:all])
       end
       subject
     end
